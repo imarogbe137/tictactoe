4bcae15c941479d1b88ac55c5e0c377f5754fe1c
NISCHAL GURUNG <nischalgurung101@gmail.com>
2023-08-05 19:47:23 +0530

    added players and winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import './styles.scss';
 import Board from './components/Board';
+import { useState } from 'react';
+import { calculateWinner } from './winner';
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setXNext] = useState(true);
+  const winner = calculateWinner(squares);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winner
+    ? `The winner is ${winner}`
+    : `The Next Player is ${nextPlayer}`;
+
+  console.log(squares);
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) return;
+
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition == position) {
+          return isXNext ? 'X' : 'O';
+        } else return squareValue;
+      });
+    });
+    setXNext(currentIsXNext => !currentIsXNext);
+  };
   return (
     <div className="app">
-      <Board />
+      <h2>{statusMessage}</h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
